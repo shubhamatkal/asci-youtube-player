f1a42be858ae053bb543b61e14fb4cb48cc72cc9
Shubham Atkal <99415769+shubhamatkal@users.noreply.github.com>
2024-06-04 20:11:52 +0530

    Update asci.sh

--- asci.sh
+++ asci.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Fetch the video URL using yt-dlp
 video_url=$(yt-dlp -f best --get-url "$1")
-
-# Play the video using mplayer with caca output in fullscreen terminal
 mplayer -screenw  1000 -vo caca "$video_url"
